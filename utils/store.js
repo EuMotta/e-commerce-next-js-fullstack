17dbde5ef12037c72ca22a2b94e49b9d2d0a81f2
EuMotta <Hyperxmotta@hotmail.com>
2022-09-16 11:56:18 -0300

    add cookies

---
+++ utils/store.js
@@ -0,0 +1,16 @@
+import Cookies from 'js-cookie'
+import  { useReducer } from 'react'
+
+const initialState = {
+    cart: Cookies.get('cart')?JSON.parse(Cookies.get('cart')):{cartItens:[]}
+}
+
+export default function StoreProvider(children) {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const value = {state, dispatch}
+    return (
+        <StoreProvider value={value}>
+            {children}
+        </StoreProvider>
+    )
+}
