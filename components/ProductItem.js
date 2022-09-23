4cea0e6470b8e84cc255856c2789448fdb21be72
EuMotta <Hyperxmotta@hotmail.com>
2022-09-23 11:26:11 -0300

    index

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -7,17 +7,21 @@ export default function ProductItem({ product }) {
     return (
         <div className="card">
             <Link href={`/product/${product.slug}`}>
+                <div className="cursor-pointer">
                     <Image
                         src={product.image}
                         alt={product.name}
                         width={350}
                         height={350}
-                        className="rounded cursor-pointer card"
+                        className="rounded card"
                     />
+                </div>
             </Link>
-            <div className="mx-3">
+            <div className="mx-2">
                 <Link href={`/product/${product.slug}`}>
+                    <a>
                         <h2 className="text-lg">{product.name}</h2>
+                    </a>
                 </Link>
                 <p className="mb-2">{product.publisher}</p>
                 <div className="flex justify-between w-full">
