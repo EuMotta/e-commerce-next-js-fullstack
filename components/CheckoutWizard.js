51bee3092003cabd1ed6bf28935f6b0eb68ab0a9
EuMotta <Hyperxmotta@hotmail.com>
2022-11-09 09:03:03 -0300

    Checkout styles change

--- components/CheckoutWizard.js
+++ components/CheckoutWizard.js
@@ -5,11 +5,13 @@ import {
     HiOutlineClipboardCheck
 } from 'react-icons/hi'
 import { FaShippingFast } from 'react-icons/fa'
+import { AiOutlineLoading } from 'react-icons/ai'
+import { useRouter } from 'next/router'
 
 const iconSteps = [
     {
-        path: 'auth',
-        display: 'autenticação',
+        path: 'login',
+        display: 'Autenticação',
         icon: HiOutlineLogin
     },
     {
@@ -23,31 +25,33 @@ const iconSteps = [
         icon: HiOutlineDocumentSearch
     },
     {
-        path: 'placeOrder',
+        path: 'placeorder',
         display: 'Revisão',
         icon: HiOutlineClipboardCheck
     },
 ]
 export default function CheckoutWizard({ activeStep = 0 }) {
-
+    const router = useRouter()
     return (
-        <div className='mb-5 flex flex-wrap'>
+        <div className='mb-5 gap-x-3 flex flex-wrap'>
             {iconSteps.map(
                 (step, index) => (
                     <div
                         key={step.path}
-                        className={`flex-1 border-b-2 text-center text-2xl 
+                        className={`flex-1 border-b-4 text-center text-2xl 
                         ${index <= activeStep
                                 ? 'border-indigo-500 text-indigo-500'
                                 : 'border-gray-400 text-gray-400'
                             }
                             `}
                     >
-                        {activeStep ? '' : ''}
-                        <span className='flex'>
+                        <span className='!flex !flex-col cursor-pointer !items-center card py-3' onClick={() => router.push(`/${step.path}`)}>
                             <span>{step.display}</span>
                             <step>
-                                <step.icon />
+                                {index == activeStep
+                                    ? <AiOutlineLoading className='rotating text-2xl' /> : <step.icon />
+                                }
+                                
                             </step>
                         </span>
                     </div>
