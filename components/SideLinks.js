bb49f462044f35b55114033c0e5cbe93e40a0a2e
EuMotta <Hyperxmotta@hotmail.com>
2022-11-13 19:58:03 -0300

    admin button > admin profile

--- components/SideLinks.js
+++ components/SideLinks.js
@@ -10,9 +10,11 @@ export default function SideLinks() {
   return (
     <div className="card  text-2xl  py-2 px-1">
       <ul className="text-center">
+      <Link href="/admin/adminProfile">
       <li className="admin-button !rounded-md hover:scale-100 cursor-pointer !px-2 !pb-3 !py-1" title="Painel do Administrador">
             <span className="relative top-0.5"> <FcBusinessman className="text-3xl"/></span>
           </li>
+          </Link>
         <hr className="mt-3"/>
         <Link href="/admin/dashboard">
           <li className="admin-button !rounded-md   cursor-pointer !px-2 !pb-3 !py-1" title="Dashboard">
