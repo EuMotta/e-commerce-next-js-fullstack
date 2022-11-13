f3182c0b5423ced6355aa024108ea6ff916635ef
EuMotta <Hyperxmotta@hotmail.com>
2022-11-12 21:34:23 -0300

    Title on links and design

--- components/SideLinks.js
+++ components/SideLinks.js
@@ -1,35 +1,38 @@
 import Link from "next/link"
 import React from "react"
-import { FaUsers } from 'react-icons/fa'
-import { FaClipboardList } from 'react-icons/fa'
-import { BsFillBagCheckFill } from 'react-icons/bs'
-import { MdDashboardCustomize } from 'react-icons/md'
+import { FcConferenceCall } from 'react-icons/fc'
+import { FcTodoList } from 'react-icons/fc'
+import { FcPaid } from 'react-icons/fc'
+import { FcComboChart } from 'react-icons/fc'
+import { FcBusinessman } from 'react-icons/fc'
 
 export default function SideLinks() {
   return (
     <div className="card  text-2xl  py-2 px-1">
       <ul className="text-center">
-        <li className="text-center cursor-pointer hover:bg-slate-800 hover:text-indigo-600  transition-all !rounded-md shadow-sm  shadow-slate-300"><i className="ri-admin-fill  text-3xl"></i></li>
+      <li className="admin-button !rounded-md hover:scale-100 cursor-pointer !px-2 !pb-3 !py-1" title="Painel do Administrador">
+            <span className="relative top-0.5"> <FcBusinessman className="text-3xl"/></span>
+          </li>
         <hr className="mt-3"/>
         <Link href="/admin/dashboard">
-          <li className="primary-button !rounded-full   cursor-pointer !px-2 !pb-3 !py-2">
-            <span className="relative top-0.5"><MdDashboardCustomize/></span>
+          <li className="admin-button !rounded-md   cursor-pointer !px-2 !pb-3 !py-1" title="Dashboard">
+            <span className="relative top-0.5"><FcComboChart className="text-3xl"/></span>
           </li>
         </Link>
         
         <Link href="/admin/orders">
-          <li className="primary-button !rounded-full cursor-pointer !px-2 !pb-3 !py-2">
-            <span className="relative top-0.5"><FaClipboardList/></span>
+          <li className="admin-button !rounded-md cursor-pointer !px-2 !pb-3 !py-1" title="Pedidos">
+            <span className="relative top-0.5"><FcTodoList className="text-3xl"/></span>
           </li>
         </Link>
         <Link href="/admin/products">
-          <li className="primary-button !rounded-full  cursor-pointer !px-2 !pb-3 !py-2">
-            <span className="relative top-0.5"> <BsFillBagCheckFill/></span>
+          <li className="admin-button !rounded-md  cursor-pointer !px-2 !pb-3 !py-1" title="Produtos">
+            <span className="relative top-0.5"> <FcPaid className="text-3xl"/></span>
           </li>
         </Link>
         <Link href="/admin/users">
-          <li className="primary-button !rounded-full cursor-pointer !px-2 !pb-3 !py-2">
-            <span className="relative top-0.5"> <FaUsers/></span>
+          <li className="admin-button !rounded-md cursor-pointer !px-2 !pb-3 !py-1" title="Usuários">
+            <span className="relative top-0.5"> <FcConferenceCall className="text-3xl"/></span>
           </li>
         </Link>
       </ul>
